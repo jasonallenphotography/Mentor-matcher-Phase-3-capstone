896d4d2cbb86f9f9b9c8fbc14cfa90163ceb1b45
David Lange & Ivy Vetor & Jaclyn Feminella & Jason Allen <pair+d-lange.ivyvetor.jacfem.jasonallenphotography@devbootcamp.com>
2016-05-12 18:35:58 -0400

    Establish Rails skeleton, build User migration, add gem pry-rails

---
+++ mentormatch/db/migrate/20160512223018_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :location
+      t.string :industry
+      t.string :picture_url, null: false
+      t.string :public_profile_url, null: false, unique: true
+      t.string :current_title
+      t.string :current_company
+      t.string :type, null: false
+      t.string :status, default: "available"
+      t.string :mission_statement
+
+      t.timestamps null: false
+    end
+  end
+end
