e3683f922ef058c7328a285fae35fd570f7338ac
David Lange & Ivy Vetor & Jaclyn Feminella & Jason Allen <pair+d-lange.ivyvetor.jacfem.jasonallenphotography@devbootcamp.com>
2016-05-12 19:22:49 -0400

    Fill out rough user controller, pending changes due to LinkedIn API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,54 @@
 class UsersController < ApplicationController
 
   def index
+    @users =  return_opposite_type(current_user)
+    render 'index'
   end
 
-  def create
-  end
 
   def new
+    @user = User.new()
   end
 
-  def edit
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to users_path
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
   end
+
 
   def show
+   @user = User.find(params[:id])
   end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
 
   def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(params[:user])
+
+    if @user.save
+      redirect_to users_path
+    else
+      render 'edit'
+    end
   end
 
+
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect '/users'
   end
 
 end
