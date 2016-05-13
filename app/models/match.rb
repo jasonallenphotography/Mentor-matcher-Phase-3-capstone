31b92972f29296ce9710186b44b42ade143b3eb8
Jason Allen <email": null>
2016-05-13 19:45:15 -0400

    update Match model with belongs tos

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,7 @@
 class Match < ActiveRecord::Base
   validates :initator_id, :receiver_id, :type, presence: true
+  # has_one conversation
+  belongs_to :initiator, source: :user
+  belongs_to :receiver, source: :user
+
 end
