87ecd26227807ebfe515774005c0b655b9bc3551
Jaclyn Feminella <email": null>
2016-05-13 15:18:07 -0400

    Add new route for finishing registration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
   get '/auth/linkedin/callback', to: 'sessions#create', as: 'sessions_create'
-  
+  get '/users/:id/finish', to: 'users#finish', as: 'users_finish'
+  # put
   root 'welcome#index'
 end
