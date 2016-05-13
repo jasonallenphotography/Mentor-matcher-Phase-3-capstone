3e12afb7947174dd7f6a51122fa78cf374beaf4f
Jaclyn Feminella <email": null>
2016-05-13 15:50:04 -0400

    User login and auth via linkedin complete, logic for finishing an incomplete profile complete, views for finishing complete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-
-
-
+  
   resources :users
   get '/auth/linkedin/callback', to: 'sessions#create', as: 'sessions_create'
   get '/users/:id/finish', to: 'users#finish', as: 'users_finish'
-  # put
+  post '/users/:id/finish', to: 'users#update'
   root 'welcome#index'
 end
