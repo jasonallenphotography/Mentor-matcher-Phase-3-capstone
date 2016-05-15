9cde571ca51d1bd35517eb8d9e75512289315a7f
Jason Allen <email": null>
2016-05-15 12:54:03 -0400

    refactor routes, and DRY up convo controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,24 +7,4 @@ class ConversationsController < ApplicationController
     @matches = current_user.accepted_matches
   end
 
-  def create
-    @conversation = Conversation.find_or_create_by(conversation_params)
-    # if Conversation.between(params[:mentor_id], params[:mentee_id]).present?
-    #   @conversation = Conversation.between(params[:mentor_id],params[:mentee_id]).first
-    # else
-    #   @conversation = Conversation.create!(conversation_params)
-    # end
-    if @conversation.save
-      redirect_to conversation_messages_path(@conversation)
-    else
-      @errors = @conversation.errors.full_messages
-      redirect_to root_path
-    end
-
-  end
-
-  private
-    def conversation_params
-      params.permit(:mentor_id, :mentee_id)
-    end
 end
