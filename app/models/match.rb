bec94a698ae486c3abb41d095298ea8d3c85ddd2
Jason Allen <email": null>
2016-05-15 18:00:25 -0400

    initial commit on branch, refactored acceptedMatch model to no longer include belongs_to

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,8 @@
 class Match < ActiveRecord::Base
   validates :initiator_id, :receiver_id, :type, presence: true
   validates_uniqueness_of :initiator_id, :scope => :receiver_id
+  validates_uniqueness_of :mentor_id, :scope => :mentee_id
+
 
   has_one :conversation
   belongs_to :initiator, class_name: 'User'
