45ef2d6bd60d20c3802c15891c931a5deaa49e94
Jason Allen <email": null>
2016-05-15 02:42:08 -0400

    Implement functional links for Match index to accept or reject matches.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,7 +1,7 @@
 class MatchesController < ApplicationController
 
   def index
-    @matches = nil # WORK IN PROGRESS, will continue for later AcceptedMatch.where(receiver_id: current_user.id) # || initiator_id: current_user.id
+    @matches = AcceptedMatch.where(receiver_id: current_user.id)  || AcceptedMatch.where(initiator_id: current_user.id)
     @matches_pending_my_decision = PendingMatch.where(receiver_id: current_user.id)
     @matches_pending_others_decision = PendingMatch.where(initiator_id: current_user.id)
   end
@@ -9,9 +9,9 @@ class MatchesController < ApplicationController
 
   def create
     @match = PendingMatch.new(params[:pending])
-    @match.conversation = Conversation.new()
+    # @match.conversation = Conversation.new()
     if @match.save
-      redirect_to '/matches#tabs-2'
+      redirect_to '/matches#tabs-3'
     else
       @errors = @match.errors.full_messages
       redirect_to user_path(current_user)
@@ -27,9 +27,9 @@ class MatchesController < ApplicationController
   def update
     @match = Match.find(params[:id])
 
-    @match.assign_attributes(params[:match])
+    @match.assign_attributes(match_params)
     if @match.save
-      redirect_to '/'
+      redirect_to '/matches#tabs-2'
     else
       @errors = @match.errors.full_messages
       render 'index'
@@ -39,7 +39,17 @@ class MatchesController < ApplicationController
   def destroy
     @match = Match.find(params[:id])
     @match.destroy
+    redirect_to '/matches#tabs-1'
   end
+
+  private
+
+    def match_params
+      params.require(:match).permit(:type,
+                                    :receiver_id,
+                                    :initiator_id)
+    end
+
 
 end
 
