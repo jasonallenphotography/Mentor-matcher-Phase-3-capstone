21b8fde61d56eb57b0865489859a249bbe9dc35b
Jason Allen <email": null>
2016-05-15 12:56:43 -0400

    Remove unnecessary comments from Convo controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,6 @@
 class ConversationsController < ApplicationController
 
   def index
-    # return_opposite_type(current_user)
-    # instead of all users, only pass users who have accepted and matched
     @conversations = current_user.conversations
     @matches = current_user.accepted_matches
   end
