a74d48c605f7e7acde597866b855427b3622534c
David Lange & Ivy Vetor <pair+d-lange.ivyvetor@devbootcamp.com>
2016-05-14 21:16:59 -0400

    send for prequest after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,34 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 20160514171723) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "mentor_id"
+    t.integer  "mentee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "interests", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-  create_table "conversations", force: :cascade do |t|
-
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-
-
-    t.integer  "mentor_id"
-    t.integer  "mentee_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
   create_table "matches", force: :cascade do |t|
@@ -47,13 +35,6 @@ ActiveRecord::Schema.define(version: 20160514171723) do
     t.string   "type",         null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "user_interests", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "interest_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -68,6 +49,12 @@ ActiveRecord::Schema.define(version: 20160514171723) do
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
+  create_table "user_interests", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "interest_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                               null: false
