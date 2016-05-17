11dba62c26167be5ef44933d22050267b74fcaa4
Jaclyn Feminella <jaclynfeminella@Jaclyns-MacBook-Pro.local>
2016-05-17 13:25:18 -0400

    Add ajax request for add an interest, remove coffee script files to remove interference

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,12 +1,12 @@
-
 $(document).ready(function() {
-  $('.add-interest-button-user-show').on('submit', function(event){
+  $('#add-interest-button-user-show').on('submit', function(event){
     event.preventDefault();
     var $target = $(event.target);
     $.ajax({
-      method: 'get',
+      method: 'post',
       url: $target.attr('href')
     }).done(function(response){
+      debugger
       $('#interest-container-user-show').append(response);
     }); // done functionality
   }); // new interest form click
