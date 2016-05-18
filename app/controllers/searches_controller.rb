a3577edf768f9beb0d5691f7220a97a78021238e
Jason Allen <email": null>
2016-05-18 18:42:36 -0400

    Account for hiding message bot users on search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,8 @@ class SearchesController < ApplicationController
   def index
     if params[:search]
       @users_of_opposite_type = return_opposite_type(current_user)
-      @users = @users_of_opposite_type.search(params[:search]).distinct
+      bots = User.where(id: 1 || 2 )
+      @users = @users_of_opposite_type.search(params[:search]).distinct - bots
     end
   end
 end
