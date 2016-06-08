f1a5c773a6bd65c9d1c992ed16d80a35bee9ca7e
David Lange <dlange8181@gmail.com>
2016-06-08 12:50:38 -0400

    Update associations between matches and conversations. Update convo controller. Update user model method to collect accepted matches conversations for display

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,7 +3,7 @@ class Match < ActiveRecord::Base
   validates_uniqueness_of :initiator_id, :scope => :receiver_id, :message => ": This match already exists! View your Requests page."
   # validates_uniqueness_of :mentor_id, :scope => :mentee_id
 
-  has_one :conversation
+  belongs_to :conversation, class_name: 'Conversation'
   belongs_to :initiator, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
   belongs_to :mentor
